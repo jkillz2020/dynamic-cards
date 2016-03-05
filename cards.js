6fdc80e542006a8307e31786d74d2ac90b864bbc
Josh Kilgore <joshkilgore@Joshs-MacBook-Pro.local>
2016-03-05 12:43:36 -0600

    created variables and elements

---
+++ cards.js
@@ -0,0 +1,6 @@
+var createCard =  document.getElementById("create");
+var deleteCard = document.getElementById("delete");
+var container = document.getElementById("container");
+var input = document.getElementById("input");
+var container = document.getElementById("container");
+var card = document.getElementsByClassName("card"); //array like
