7a721a71ac69ccd77def8a097df724b8b0def870
Josh Kilgore <joshkilgore@Joshs-MacBook-Pro.local>
2016-03-07 20:07:41 -0600

    add dynamic delete buttons which act to delete only one card at a time

--- cards.js
+++ cards.js
@@ -1,6 +1,32 @@
-var createCard =  document.getElementById("create");
-var deleteCard = document.getElementById("delete");
+var create =  document.getElementById("create");
+var deleteCard = document.getElementsByClassName("delete");
 var container = document.getElementById("container");
 var input = document.getElementById("input");
 var container = document.getElementById("container");
-var card = document.getElementsByClassName("card"); //array like
+var card = document.getElementsByClassName("card"); //array like object
+var text = document.getElementsByClassName("text");
+
+create.addEventListener("click", createCard );
+
+
+function createCard(){
+  var output = '<div class = "card"><section><p class = "text">' + input.value + '</p></section><input type = "button" class = "delete" value = "delete"></div>'
+    container.innerHTML += output
+    deleteBtnEventListener();
+}
+
+//target the parent card and instruct to remove child with each click of each delete button//////
+function removeCard(event){
+var thisCard = event.target.parentElement;
+container.removeChild(thisCard)  ////used node.removeChild method
+}
+////made event listener on each dynamically created delete button
+function deleteBtnEventListener(){
+  for(var i = 0; i < deleteCard.length; i++){
+    var del = document.getElementsByClassName("delete")
+      for(var d = 0; d < del.length; d++){
+        del[d].addEventListener("click", removeCard);
+      }
+  }
+  console.log(del);
+}
